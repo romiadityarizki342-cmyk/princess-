f7306ba29d8f308e07976136f8e65c32cc6624d5
romiadityarizki342-cmyk <romiadityarizki342@gmail.com>
2026-07-02 04:32:23 +0800

    Create style.css

---
+++ style.css
@@ -0,0 +1,156 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:'Poppins',sans-serif;
+}
+
+body{
+    background:linear-gradient(135deg,#ffb6d9,#ffd6ec,#fff0f7);
+    color:white;
+    overflow-x:hidden;
+    text-align:center;
+}
+
+section{
+    min-height:100vh;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    padding:30px;
+}
+
+.hero{
+    background:linear-gradient(135deg,#ff8fb1,#ffb6d9);
+}
+
+h1{
+    font-size:50px;
+    margin-bottom:15px;
+    text-shadow:0 0 15px rgba(255,255,255,.8);
+}
+
+h2{
+    margin-bottom:20px;
+}
+
+p{
+    max-width:700px;
+    font-size:18px;
+    line-height:1.8;
+}
+
+button{
+    margin-top:30px;
+    padding:15px 35px;
+    border:none;
+    border-radius:50px;
+    background:white;
+    color:#ff4f8b;
+    font-size:18px;
+    cursor:pointer;
+    transition:.3s;
+    font-weight:bold;
+}
+
+button:hover{
+    transform:scale(1.08);
+}
+
+.hidden{
+    display:none;
+}
+
+.timer{
+    display:flex;
+    gap:20px;
+    margin-top:30px;
+    flex-wrap:wrap;
+    justify-content:center;
+}
+
+.timer div{
+    background:rgba(255,255,255,.2);
+    backdrop-filter:blur(10px);
+    border-radius:20px;
+    padding:20px;
+    min-width:90px;
+}
+
+.timer span{
+    font-size:35px;
+    font-weight:bold;
+}
+
+.slider{
+    display:flex;
+    gap:20px;
+    overflow-x:auto;
+    padding:20px;
+    scroll-snap-type:x mandatory;
+}
+
+.slider img,
+.slider video{
+    width:280px;
+    border-radius:20px;
+    scroll-snap-align:center;
+    box-shadow:0 10px 25px rgba(0,0,0,.3);
+}
+
+#cakeEmoji{
+    font-size:80px;
+    margin:30px;
+}
+
+.hearts{
+    position:fixed;
+    inset:0;
+    pointer-events:none;
+}
+
+.hearts::before{
+    content:"💖 💕 💗 💞 ❤️";
+    position:absolute;
+    width:100%;
+    animation:turun 12s linear infinite;
+    font-size:30px;
+}
+
+@keyframes turun{
+
+0%{
+transform:translateY(-100%);
+}
+
+100%{
+transform:translateY(120vh);
+}
+
+}
+
+.letter,
+.gallery,
+.cake,
+.ending,
+.countdown{
+    background:linear-gradient(135deg,#ff9fc5,#ffcbe3);
+}
+
+@media(max-width:600px){
+
+h1{
+font-size:36px;
+}
+
+.slider img,
+.slider video{
+width:230px;
+}
+
+.timer{
+gap:10px;
+}
+
+}
